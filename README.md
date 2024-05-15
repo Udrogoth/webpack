7b010dc2423177f5344ca66c66de207917837909
Daniel Tobar Calderón <daniel-tobarc@hotmail.com>
2024-05-15 14:45:04 -0600

    inicializando

---
+++ README.md
@@ -0,0 +1,13 @@
+# webpack Starter
+Este es el proyecto inicial para clear aplicacion
+utilizando webpack.
+
+## Notas 
+Recostruir los módulos
+```
+npm install
+```
+Para construit el build: 
+```
+npm run build
+```
---
+++ webpack.prod.js
@@ -0,0 +1,77 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    output: {
+        filename: 'main.[contenthash].js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                exclude: /style\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /style\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+                options: {
+                    // Disables attributes processing
+                    sources: false,
+                    minimize: false,
+                },
+            },
+
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test: /\.png|jpg$/i,
+            //     loader: 'file-loader',
+            //     options: {
+            //         esModule: false,
+            //         name: 'assets/[name].[ext]'
+            //     }
+
+
+            // },
+
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            ignoreOrder: false
+
+        }),
+
+        
+    ]
+
+};
